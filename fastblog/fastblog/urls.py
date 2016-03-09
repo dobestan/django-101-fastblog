9b4d4a7ff5608080b23a2bd90888c8c3ed66687f
Suchan An <dobestan@gmail.com>
2016-03-09 22:09:05 +0900

    init home view: FastblogViewTestCase passed

--- fastblog/fastblog/urls.py
+++ fastblog/fastblog/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from fastblog.views import home
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    url(r'^$', home, name="home"),
 ]
