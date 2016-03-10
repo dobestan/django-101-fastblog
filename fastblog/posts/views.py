3c331c69622541a27912abaa115fa198ef9996db
Suchan An <dobestan@gmail.com>
2016-03-10 22:18:07 +0900

    refactor PostListView, PostDetailView: inherit from PostBaseView

--- fastblog/posts/views.py
+++ fastblog/posts/views.py
@@ -1,14 +1,16 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import View, ListView, DetailView
 
 from posts.models import Post
 
 
-class PostListView(ListView):
+class PostBaseView(View):
     model = Post
+
+
+class PostListView(PostBaseView, ListView):
     template_name = "posts/list.html"
     context_object_name = "posts"
 
 
-class PostDetailView(DetailView):
-    model = Post
+class PostDetailView(PostBaseView, DetailView):
     template_name = "posts/detail.html"
