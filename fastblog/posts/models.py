f8f88dba1f7a4543c163ff09febb81ce7741d671
Suchan An <dobestan@gmail.com>
2016-03-12 15:21:32 +0900

    init Comment Model: 1:N Relationship to Post Model

--- fastblog/posts/models.py
+++ fastblog/posts/models.py
@@ -9,3 +9,16 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey("Post")
+    content = models.CharField(
+        max_length=100,
+    )
+
+    def __str__(self):
+        return 'Commented "{comment_content}" on "{post_title}"'.format(
+            comment_content=self.content,
+            post_title=self.post.title,
+        )
