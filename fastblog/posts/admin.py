41cca9b6da8e411e287d5a64e1d877650dbeda4c
Suchan An <dobestan@gmail.com>
2016-03-14 22:03:11 +0900

    update PostModelAdmin, CommentModelAdmin: create custom ModelAdmin

--- fastblog/posts/admin.py
+++ fastblog/posts/admin.py
@@ -3,5 +3,53 @@ from django.contrib import admin
 from posts.models import Post, Comment
 
 
-admin.site.register(Post)
-admin.site.register(Comment)
+
+class PostModelAdmin(admin.ModelAdmin):
+
+    list_display = admin.ModelAdmin.list_display + (
+        'title',
+        'content',
+
+        'is_public',
+
+        'created_at',
+        'updated_at',
+    )
+
+    list_filter = admin.ModelAdmin.list_filter + (
+        'is_public',
+
+        'created_at',
+        'updated_at',
+    )
+
+    search_fields = (
+        'title',
+        'content',
+    )
+
+
+class CommentModelAdmin(admin.ModelAdmin):
+
+    list_display = admin.ModelAdmin.list_display + (
+        'content',
+
+        'created_at',
+        'updated_at',
+    )
+
+    list_filter = admin.ModelAdmin.list_filter + (
+        'created_at',
+        'updated_at',
+    )
+
+    search_fields = (
+        'content',
+
+        'post__title',
+        'post__content',
+    )
+
+
+admin.site.register(Post, PostModelAdmin)
+admin.site.register(Comment, CommentModelAdmin)
