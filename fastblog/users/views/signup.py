168a9b0264903187f93fd7ae75241fe8295f3e6c
Suchan An <dobestan@gmail.com>
2016-03-17 00:59:29 +0900

    update SignupView: add automatic login feature

--- fastblog/users/views/signup.py
+++ fastblog/users/views/signup.py
@@ -2,6 +2,7 @@ from django.views.generic import View
 from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
 
 
 class SignupView(View):
@@ -21,5 +22,12 @@ class SignupView(View):
             username=username,
             password=password,
         )
+
+        # 회원가입 후 자동 로그인 구현하기
+        user = authenticate(
+            username=username,
+            password=password,
+        )
+        login(request, user)
 
         return redirect(reverse("home"))
