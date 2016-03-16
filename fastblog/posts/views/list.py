b69d84b8f934ab97c902403e340389022632cace
Suchan An <dobestan@gmail.com>
2016-03-17 00:19:34 +0900

    update PostListView: add pagination feature with posts_per_page support

--- fastblog/posts/views/list.py
+++ fastblog/posts/views/list.py
@@ -3,24 +3,9 @@ from django.views.generic import ListView
 from .base import PostBaseView
 
 
-# class PostListView(PostBaseView, ListView):
-#     template_name = "posts/list.html"
-#     context_object_name = "posts"
+class PostListView(PostBaseView, ListView):
+    template_name = "posts/list.html"
+    context_object_name = "posts"
 
-def posts(request):
-    page = request.GET.get('page', 1)
-    posts_per_page = request.GET.get('per', 3)
-
-    paginator = Paginator(
-        Post.objects.all(),
-        posts_per_page,
-    )
-    posts = paginator.page(page)
-
-    return render(
-        request,
-        "posts/list.html",
-        context={
-            'posts': posts,
-        }
-    )
+    def get_paginate_by(self, queryset):
+        return self.request.GET.get('per', 3)
