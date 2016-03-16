ec235ef5615de9f895c5eaa08edfcff91406a122
Suchan An <dobestan@gmail.com>
2016-03-17 01:31:44 +0900

    update LoginRequiredView: add login_required feature in both FBV, CBV

--- fastblog/users/views/login_required.py
+++ fastblog/users/views/login_required.py
@@ -3,8 +3,12 @@ login_required decorator usage in FBV, CBV.
 """
 from django.views.generic import View, TemplateView
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
+@login_required()
 def login_required_view(request):
     return render(
         request,
@@ -13,5 +17,10 @@ def login_required_view(request):
     )
 
 
-class LoginRequiredView(TemplateView):
+# @method_decorator(login_required, name='dispatch')
+class LoginRequiredView(LoginRequiredMixin, TemplateView):
     template_name = "users/login_required.html"
+
+    # @method_decorator(login_required)
+    # def dispatch(self, *args, **kwargs):
+    #     return super(LoginRequiredView, self).dispatch(*args, **kwargs)
