f58407cb36893068d71dc4073e250bbf1a71d14a
Suchan An <dobestan@gmail.com>
2016-03-17 03:11:36 +0900

    refactor signals: add signals module in users application

--- fastblog/users/models/user_profile.py
+++ fastblog/users/models/user_profile.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 
 class UserProfile(models.Model):
@@ -23,11 +21,3 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.__str__()
-
-
-@receiver(post_save, sender=User)
-def post_save_user(sender, instance, created, **kwargs):
-    if created:
-        user_profile = UserProfile.objects.create(
-            user=instance,
-        )
