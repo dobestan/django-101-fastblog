c3e147878e18f1332023a5343e443c0fbe363bb5
Suchan An <dobestan@gmail.com>
2016-03-24 01:50:56 +0900

    update posts detail page: add ajax loading spinner

--- fastblog/fastblog/static/js/posts/detail.js
+++ fastblog/fastblog/static/js/posts/detail.js
@@ -2,6 +2,14 @@
   "use strict";
 
   $(document).ready(function(){
+    $(document).ajaxStart(function(){
+      $("i#comments-spinner").show();
+    }).ajaxStop(function(){
+      $("i#comments-spinner").hide();
+    });
+  });
+
+  $(document).ready(function(){
     var comment_element = $("ul#comments");
     var post_id = comment_element.data("post-id");
 
