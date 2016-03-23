d7aa0e805896ebc82931d2296da5ffb391ee0ea6
Suchan An <dobestan@gmail.com>
2016-03-24 01:40:37 +0900

    update posts detail page: load comments after page loaded

---
+++ fastblog/fastblog/static/js/posts/detail.js
@@ -0,0 +1,21 @@
+(function(){
+  "use strict";
+
+  $(document).ready(function(){
+    var comment_element = $("ul#comments");
+    var post_id = comment_element.data("post-id");
+
+    $.ajax({
+      url: "/api/posts/" + post_id + "/comments/",
+      type: "GET",
+      success: function(comments){
+        for (var i = 0; i < comments.length; i++) {
+          var comment = comments[i];
+          comment_element.append(
+            "<li>" + comment.content + "</li>"
+          );
+        }
+      }
+    });
+  });
+})();
