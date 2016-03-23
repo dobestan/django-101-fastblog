0370a84f6f9b13d59550ed7951d370596c11b193
Suchan An <dobestan@gmail.com>
2016-03-23 20:59:02 +0900

    init social login: FacebookOAuth2, KakaoOAuth2

--- fastblog/fastblog/urls.py
+++ fastblog/fastblog/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 from fastblog.views import home
@@ -27,6 +27,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    url('', include('social.apps.django_app.urls', namespace='social')),
 
     url(r'^$', home, name="home"),
     url(r'^posts/$', PostListView.as_view(), name="posts"),
